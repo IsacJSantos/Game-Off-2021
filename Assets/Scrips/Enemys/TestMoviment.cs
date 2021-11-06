a0e23f6f03fdc1b6cac1f483ce5e547caaf24700
Isac <isac4322@gmail.com>
2021-11-05 21:37:54 -0300

    Enemys look forward

--- Assets/Scrips/Enemys/TestMoviment.cs
+++ Assets/Scrips/Enemys/TestMoviment.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,19 +5,12 @@ public class TestMoviment : MonoBehaviour
 {
     [SerializeField] NavMeshAgent agent;
     [SerializeField] Transform destination;
-    // Start is called before the first frame update
-    void Start()
-    {
-        agent.updateRotation = false;
-        agent.updateUpAxis = false;
-        agent.SetDestination(destination.position);
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
-        agent.destination = destination.position;
-        //transform.LookAt(agent.steeringTarget, Vector3.up);
-       // transform.rotation = Quaternion.Euler(0, 0, transform.rotation.z);
+        agent.SetDestination(destination.position);    
     }
+
+    
 }
