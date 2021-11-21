bd7b3bf6b9398b215c6733c66cd4a1c83a290e2d
Isac <isac4322@gmail.com>
2021-11-21 19:31:09 -0300

    Try fix

--- Assets/Scrips/TesteNoInimigo.cs
+++ Assets/Scrips/TesteNoInimigo.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class TesteNoInimigo : MonoBehaviour
 {
-    public MouseCursor newCursor;
+   // public MouseCursor newCursor;
 
 
     void OnMouseOver()
     {
         // If the mouse hover over the GameObject with the script attached
         Debug.Log("Mouse is hovering enemy");
-        newCursor.ChangeCursor();
+       // newCursor.ChangeCursor();
     }
 
     void OnMouseExit()
     {
         //The mouse is no longer hovering over the GameObject
         Debug.Log("Mouse is no longer on enemy");
-        newCursor.DefaultCursor();
+       // newCursor.DefaultCursor();
     }
 }
