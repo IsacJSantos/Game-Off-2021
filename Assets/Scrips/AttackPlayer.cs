7c0f16ab5c83bd2512ac98e5e25f3e69d1a452e6
Júlia Barreto <barretojulia@id.uff.br>
2021-11-21 15:46:24 -0300

    update

--- Assets/Scrips/AttackPlayer.cs
+++ Assets/Scrips/AttackPlayer.cs
@@ -7,7 +7,7 @@ public class AttackPlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //comentario para teste
     }
 
     // Update is called once per frame
