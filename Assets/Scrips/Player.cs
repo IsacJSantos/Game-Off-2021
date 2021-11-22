55bf1d246138050fd4bbc06cfc6a9ca61f2ae13c
Isac <isac4322@gmail.com>
2021-11-22 20:55:37 -0300

    Moving player with bug

--- Assets/Scrips/Player.cs
+++ Assets/Scrips/Player.cs
@@ -7,30 +7,37 @@ public class Player : MonoBehaviour
     Vector3 movement;
     [SerializeField] float speed;
     float addAngle = 270.0f;
+    [SerializeField] Rigidbody rb;
 
     private void Update()
     {
         ProcessInputs();
-        //AimAndShoot();
         Move();
+        //AimAndShoot();
+
     }
 
     private void ProcessInputs()
     {
         //coletar inputs do movimento do personagem
         movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-        movement = Vector3.ClampMagnitude(movement, 1.0f); //prevenir bug da velocidade maior na diagonal
+        movement = Vector3.ClampMagnitude(movement, 1.0f) * speed * Time.deltaTime; //prevenir bug da velocidade maior na diagonal
+
+        if (movement == Vector3.zero)
+            rb.velocity = Vector3.zero;
 
         //coletar inputs da mira
         Vector3 aimPos = Input.mousePosition - Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, 0.0f, transform.position.z));
         float angle = Mathf.Atan2(aimPos.y, aimPos.x) * Mathf.Rad2Deg;
         angle = angle + addAngle;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.down);
+       
     }
-
+   
     private void Move()
     {
         //movimentar personagem
-        transform.position = transform.position + movement * Time.deltaTime * speed;
+        rb.velocity = movement;
+        //transform.position = transform.position + movement * Time.fixedDeltaTime * speed;
     }
 }
