8a467ca156c3ce99602dc2f87b6ece7a8be701d3
les9814 <les9814@users.noreply.github.com>
2021-11-22 20:08:22 -0300

    Limitar a velocidade do personagem
    Limitar a velocidade do personagem na horizontal/vertical/diagonal e limpar o código

--- Assets/Scrips/Player.cs
+++ Assets/Scrips/Player.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
     Vector3 movement;
     [SerializeField] float speed;
-    [SerializeField] float addAngle = 270.0f;
+    float addAngle = 270.0f;
 
     private void Update()
     {
@@ -18,15 +17,13 @@ public class Player : MonoBehaviour
 
     private void ProcessInputs()
     {
-        //coletar inputs para movimento do personagem
+        //coletar inputs do movimento do personagem
         movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        movement = Vector3.ClampMagnitude(movement, 1.0f); //prevenir bug da velocidade maior na diagonal
 
-        //alinhar a rotação do personagem conforme a mira
-        //https://www.youtube.com/watch?v=Geb_PnF1wOk&t=63s
-        //https://stackoverflow.com/questions/59234157/make-character-look-at-mouse-in-unity
-
-        var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, 0.0f, transform.position.z));
-        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        //coletar inputs da mira
+        Vector3 aimPos = Input.mousePosition - Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, 0.0f, transform.position.z));
+        float angle = Mathf.Atan2(aimPos.y, aimPos.x) * Mathf.Rad2Deg;
         angle = angle + addAngle;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.down);
     }
