2dd25bfc3ab7121b86b3689654da3d22a2a6a246
juliab <s.barretojulia@gmail.com>
2021-11-28 00:15:26 -0300

    Fazendo barra de vida

---
+++ Assets/LifeBar.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetLife()
+    {
+
+    }
+}
